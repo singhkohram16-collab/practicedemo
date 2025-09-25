94e019818c00de92c084128e0ae30eaf9e0268db
kohrambhau <singhkohram16@gmail.com>
2025-09-25 13:35:07 +0530

    initial commit by primary branch

--- main.py
+++ main.py
@@ -4,3 +4,10 @@ if number >= 18:
     print("You are eligible to drive a car")
 else:
     print("You are not eligible to drive a car")
+
+name = str(input("Enter your name : "))
+
+if name.lower() == "amit":
+    print("You are good")
+else:
+    print("You are bad")
